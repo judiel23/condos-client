5ff01535d57f94a338c52a20a74b9884719bf183
judiel23 <judielsm@gmail.com>
2016-01-17 00:23:56 -0430

    app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name condosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the condosApp
+ */
+angular.module('condosApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name condosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the condosApp
+ */
+angular.module('condosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
